6909fc7cb37661ba381ad23768abbdbf533749f1
DaveJay3002 <davejay3002@gmail.com>
2022-08-10 11:01:53 +0530

    Completed Stack implementation

--- Stack.cpp
+++ Stack.cpp
@@ -8,62 +8,95 @@
 
 #include <stdlib.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-template<class X> class Stack{
+template <class X> class Stack{
+    int tos,capacity;
+    X *stack;
     public:
-    int tos,SIZE=10;
-    X stack[SIZE];
-    Stack(int x){SIZE=x;tos=0;X stack[SIZE];}
-    // Stack(){SIZE=10;tos=-1;X stack[SIZE];}
-    int isFull(){
-        if (tos==SIZE-1) return 1;
-        else return 0;
+    Stack(int);
+    void push(X);
+    X pop();
+    X peek();
+    int size();
+    bool isEmpty();
+    bool isFull();
+    void show();
+    ~Stack(){
+        delete[] stack;
     }
-    int isEmpty(){
-        if (tos==-1) return 1;
-        else return 0;
-    }
-
-    void push(X obj){
-        if (this->isFull()) cout<<"Stack Overflow"<<endl;
-        else{
-            stack[tos]=obj;
-            this->tos++;
-        }
-    }
-
-    X pop(){
-        if (this->isEmpty()) {
-            cout<<"Stack Underflow"<<endl;
-            return 0;
-        }
-        else{
-            this->tos--;
-            return stack[tos+1];
-        }
-    }
-
-    X peek(){
-        return stack[tos];
-    }
-
-    string printStack(){
-        string out;
-        for (int i=this->tos;i>-1;--i){
-            out=out+stack[i]+' ';
-        }
-        out+='\n';
-        return out;
-    }
-
 };
 
+template <class X> Stack<X>::Stack(int size){
+    stack=new X[size];
+    capacity=size;
+    tos=-1;
+}
+
+template <class X> void Stack<X>::push(X ele){
+    if(isFull()){
+        cout<<"Stack Overflow"<<endl;
+        return;
+    }
+    stack[++tos]=ele;
+}
+
+template <class X> X Stack<X>::pop(){
+    if (isEmpty()){
+        cout<<"Stack Underflow"<<endl;
+        return 0;
+    }
+    return stack[tos--];
+}
+
+template <class X> X Stack<X>::peek(){
+    if (!isEmpty()){
+        return stack[tos];
+    }
+    else{
+        cout<<"Stack Underflow"<<endl;
+        return 0;
+    }
+}
+
+template <class X> int Stack<X>::size(){
+    return tos+1;
+}
+
+template <class X> bool Stack<X>::isEmpty(){
+    if (tos==-1) return true;
+    else return false;
+}
+
+template <class X> bool Stack<X>::isFull(){
+    if (tos==capacity-1) return true;
+    else return false;
+}
+
+template <class X> void Stack<X>::show(){
+    for (int i=tos;i>-1;i--){
+        cout<<stack[i]<<endl;
+    }
+    return;
+}
+
 int main(){
-    Stack <int> S1(15);
-    S1.push(17);
-    S1.push(18);
-    cout<<S1.printStack();
+    Stack <int> int_stack(5);
+    Stack <string> str_stack(5);
+    int_stack.push(10);
+    int_stack.push(11);
+    int_stack.push(12);
+    int_stack.push(13);
+    int_stack.push(14);
+    // int_stack.show();
+    int_stack.pop();
+    int_stack.pop();
+    // int_stack.show();
+    str_stack.push("Jay");
+    str_stack.push("Dave");
+    str_stack.show();
+    cout<<str_stack.size()<<endl;
     return 0;
 }
