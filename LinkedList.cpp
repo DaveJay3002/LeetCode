723d8f46d9351057b1d7501279c571d127bb48b6
DaveJay3002 <davejay3002@gmail.com>
2022-08-11 22:55:00 +0530

    Started to implement Linked List

---
+++ LinkedList.cpp
@@ -0,0 +1,16 @@
+// Traversal - access each element of the linked list
+// Insertion - adds a new element to the linked list
+// Deletion - removes the existing elements
+// Search - find a node in the linked list
+// Sort - sort the nodes of the linked list
+
+#include <stdlib.h>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <class X> struct node{
+    X data;
+    node* next;
+};
